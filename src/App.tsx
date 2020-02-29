d56e78b7349e2748534c2d40ecd3f4d49a2cb75a
krzysztof <krzysztofkrych2@gmail.com>
2020-02-29 11:05:57 +0100

    Provider for app file

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import css from './App.module.scss';
+import { Provider } from 'react-redux';
+import { store } from './store/config';
 
 function App() {
   return (
-    <div className={css.App}>
-     
-    </div>
+    <Provider store={store}>
+      <div className={css.App}>
+        testest
+      </div>
+    </Provider>
   );
 }
 
